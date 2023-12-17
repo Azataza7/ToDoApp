19466877558c711d4195df90def641d05eeb2cc0
Azat Kulbaev <kulbaevaazat@gmail.com>
2023-12-17 22:38:19 +0600

    commit #1 starting project with necessary dependecies and components

---
+++ src/Containers/TaskList/TaskListSlice.ts
@@ -0,0 +1,9 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+
+const TaskListSlice = createSlice({
+
+})
+
+export const taskReducer = TaskListSlice.reducer;
+export const {} = TaskListSlice.actions;
